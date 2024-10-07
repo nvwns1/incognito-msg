acfb0b3b020c3c8004aa54d7093d9985b5234d74
nvwns1 <nvwns12345@gmail.com>
2024-10-07 16:23:25 +0545

    Git merging issue solved

--- src/lib/dbConnection.ts
+++ src/lib/dbConnection.ts
@@ -8,7 +8,6 @@ const connection: ConnectionObject = {};
 
 async function dbConnection(): Promise<void> {
   if (connection.isConnected) {
-<<<<<<< HEAD
     console.log("Database already connected");
     return;
   }
@@ -22,19 +21,6 @@ async function dbConnection(): Promise<void> {
     console.log("Database connected");
   } catch (error) {
     console.error("Error connecting to database", error);
-=======
-    console.log("Database is already connected");
-    return;
-  }
-  try {
-    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
-    console.log({ db });
-    connection.isConnected = db.connections[0].readyState;
-    console.log({ connection });
-    console.log("Database connected Successfully");
-  } catch (error) {
-    console.log("Error connecting to database", error);
->>>>>>> 3f31b618e627b117f5778ffab5b3b529932a5bf9
     process.exit(1);
   }
 }
--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,7 +1,3 @@
 import { Resend } from "resend";
 
-<<<<<<< HEAD
 export const resend = new Resend(process.env.RESNED_API_KEY);
-=======
-export const resend = new Resend(process.env.RESEND_API_KEY);
->>>>>>> 3f31b618e627b117f5778ffab5b3b529932a5bf9
