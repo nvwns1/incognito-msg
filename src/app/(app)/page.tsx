4b3856022232d538a965de7786e07efa9854a6c9
nvwns1 <nvwns12345@gmail.com>
2024-10-09 10:57:48 +0545

    refactor layout

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  // TODO: Add your own code here
+  return (
+    // THis is home Page
+    <div>this is home page</div>
+  );
+}
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,22 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Incognito Message",
   description: "",
 };
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export default function RootLayout({
   children,
@@ -14,17 +25,23 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <main className="flex items-center justify-center min-h-screen bg-gray-100">
-        <div className="w-full max-w-md p-8 space-y-8 bg-white shadow-md rounded-lg">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight lg:5xl mb-6">
-              Incognito Message
-            </h1>
-          </div>
-          {children}
-          <Toaster />
-        </div>
-      </main>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <main className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-8 bg-white shadow-md rounded-lg">
+              <div className="text-center">
+                <h1 className="text-4xl font-bold tracking-tight lg:5xl mb-6">
+                  Incognito Message
+                </h1>
+              </div>
+              {children}
+              <Toaster />
+            </div>
+          </main>
+        </body>
+      </html>
     </>
   );
 }
